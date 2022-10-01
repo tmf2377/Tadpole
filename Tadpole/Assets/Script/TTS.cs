f6d497bbcd7bf57d3698d69230956d1bc399ae0d
seoa <tmf2377@naver.com>
2022-10-02 01:42:14 +0900

    설정, 나가기 Panel 세팅

---
+++ Tadpole/Assets/Script/TTS.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class TTS : MonoBehaviour
+{
+    Text flashingText;
+
+    void Start() { 
+        flashingText = GetComponent<Text>(); 
+        StartCoroutine(BlinkText()); 
+    }
+
+
+    public IEnumerator BlinkText() { 
+        while (true) { 
+            flashingText.text = ""; 
+            yield return new WaitForSeconds(.5f); 
+            flashingText.text = "Tab to Start"; 
+            yield return new WaitForSeconds(.5f); 
+        } 
+    }
+
+    
+}
