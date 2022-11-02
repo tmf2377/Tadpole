903637bb1b324cd0ca1f70d141e65e0dbd8025ef
leech1211 <leech1211@nate.com>
2022-11-02 23:34:44 +0900

    UI 수정,스테이지 구성, 먹이 오브젝트 생성

---
+++ Tadpole/Assets/Script/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform target;
+    public float speed;
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        //transform.position = new Vector3(target.position.x, target.position.y, -10f);
+        transform.position = Vector3.Lerp(transform.position, target.position, Time.deltaTime * speed);
+        transform.position = new Vector3(transform.position.x, transform.position.y, -10f);
+    }
+}
