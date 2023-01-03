d52ff0ade80e47b6bba9e770ccba9daad7a564f5
seoa <tmf2377@naver.com>
2023-01-04 00:23:29 +0900

    시작화면,올챙이속도,개구리이슈

--- Tadpole/Assets/Script/FrogAction.cs
+++ Tadpole/Assets/Script/FrogAction.cs
@@ -86,9 +86,10 @@ public class FrogAction : MonoBehaviour
         //Landing Platform
         if(rigid.velocity.y<0){
             Debug.DrawRay(rigid.position, Vector3.down, new Color(0,1,0));
+            // Grid에서 Layer가 Platform으로 되어있어야함
             RaycastHit2D rayHit = Physics2D.Raycast(rigid.position, Vector3.down, 1, LayerMask.GetMask("Platform"));
             if(rayHit.collider != null){
-                if(rayHit.distance < 0.5f)
+                if (rayHit.distance < 0.5f)
                 {
                     anim.SetBool("isJumping", false);
                 }
