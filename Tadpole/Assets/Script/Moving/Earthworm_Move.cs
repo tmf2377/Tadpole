79a2c1ffcf1c872cd3777b54a1a9c0e81046e996
leech1211 <leech1211@nate.com>
2023-02-07 19:33:32 +0900

    NPC 움직임 및 충돌 etc

--- Tadpole/Assets/Script/Moving/Earthworm_Move.cs
+++ Tadpole/Assets/Script/Moving/Earthworm_Move.cs
@@ -7,15 +7,15 @@ public class Earthworm_Move : MonoBehaviour
     Rigidbody2D rigid;      //물리를 받아야 하기에 리지드바디 추가
     public float nextMove;        //행동지표를 결정할 변수 
     SpriteRenderer spriteRenderer;  //문워크 방지
+    float temp; //좁은 땅 도리도리 방지
 
     // Start is called before the first frame update
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();    //초기화
         spriteRenderer = this.GetComponent<SpriteRenderer>();
-
-        //주어진 시간이 지난 뒤 지정된 함수를 실행 : Invoke()
-        Invoke("Think", 5); // 5초마다  Think() 실행
+        temp = 0;  
+        Think();
     }
 
     // Update is called once per frame
@@ -27,12 +27,17 @@ public class Earthworm_Move : MonoBehaviour
 
         //방향전환
         if (nextMove < 0)
-            spriteRenderer.flipX = false;
+        {
+            spriteRenderer.flipX = false; temp = -0.5f;
+        }            
         if (nextMove > 0)
-            spriteRenderer.flipX = true;
+        {
+            spriteRenderer.flipX = true; temp = 0.5f;
+        }
+            
 
         //Platform Check
-        Vector2 frontVec = new Vector2(rigid.position.x + nextMove, rigid.position.y);      //x축은 자기 자신의 앞을,y축은 자기 자신을 미리 본다
+        Vector2 frontVec = new Vector2(rigid.position.x + temp, rigid.position.y);      //x축은 자기 자신의 앞을,y축은 자기 자신을 미리 본다
         Debug.DrawRay(frontVec, Vector3.down, new Color(0, 1, 0));
         RaycastHit2D rayHit = Physics2D.Raycast(frontVec, Vector3.down, 1, LayerMask.GetMask("Platform"));
         if (rayHit.collider == null)
@@ -47,9 +52,8 @@ public class Earthworm_Move : MonoBehaviour
     //행동지표를 바꿔줄 메서드 //reculsive
     void Think()
     {
-        nextMove = Random.Range(-1f, 1f);    //랜덤수의 범위
+        nextMove = Random.Range(-1, 2);    //랜덤수의 범위
                                            //최소값은 범위에 포함되지만 최대값은 포함되지 않는다
-
         Invoke("Think", 2);
     }
 }
