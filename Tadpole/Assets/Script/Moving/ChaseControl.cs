cab8c102733c60b8c041d151a88d333cc1e36921
leech1211 <leech1211@nate.com>
2023-02-09 00:41:08 +0900

    잠자리 움직임

---
+++ Tadpole/Assets/Script/Moving/ChaseControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseControl : MonoBehaviour
+{
+    public dragonfly_Move[] enemyArray;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            foreach(dragonfly_Move dm in enemyArray)
+            {
+                dm.chase = true;
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            foreach(dragonfly_Move dm in enemyArray)
+            {
+                dm.chase = false;
+            }
+        }
+    }
+}
+
+
