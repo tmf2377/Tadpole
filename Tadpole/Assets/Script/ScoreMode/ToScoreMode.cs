20e1dacb54150488dad237a5ec5c89de89a2172f
leech1211 <leech1211@nate.com>
2023-02-10 02:20:29 +0900

    스코어모드 세팅

---
+++ Tadpole/Assets/Script/ScoreMode/ToScoreMode.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToScoreMode : MonoBehaviour
+{
+    public void SceneChange()
+    {
+        SceneManager.LoadScene("SelectScene");
+    }
+   
+}
