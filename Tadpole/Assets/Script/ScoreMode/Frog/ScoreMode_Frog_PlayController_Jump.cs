cd24510d3ca2bbbf9c5f9d1e915241d79efa5465
leech1211 <leech1211@nate.com>
2023-02-10 23:54:46 +0900

    스코어모드 개구리 점프 및 타일이동

---
+++ Tadpole/Assets/Script/ScoreMode/Frog/ScoreMode_Frog_PlayController_Jump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreMode_Frog_PlayController_Jump : MonoBehaviour
+{
+   
+    public Rigidbody2D rigid;
+    public float jumpPower;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody2D>();
+        jumpPower = 6;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "ScoreMode_Frog_Enemy")
+        {
+            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            Debug.Log("Ads");
+        }
+    }
+}
