e0847e393783d0a463be882177044d896f9f5ca5
seoa <tmf2377@naver.com>
2023-02-20 23:09:48 +0900

    올챙이 적 AI

---
+++ Tadpole/Assets/Script/ScoreTadpole/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject fishPrefab;
+    [SerializeField]
+    private GameObject sharkPrefab;
+
+    [SerializeField]
+    private float fishInterval = 3.5f;
+    [SerializeField]
+    private float sharkInterval = 10f;
+
+    void Start()
+    {
+        StartCoroutine(spawnEnemy(fishInterval, fishPrefab));
+        StartCoroutine(spawnEnemy(sharkInterval, sharkPrefab));
+    }
+
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
+    {
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-25f, 25), 10, 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+    }
+}
