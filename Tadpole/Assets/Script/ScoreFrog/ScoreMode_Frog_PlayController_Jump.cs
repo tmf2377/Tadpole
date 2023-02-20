46bcfa4cc6e0eda2b3756973257c01e22d2927b2
leech1211 <leech1211@nate.com>
2023-02-20 16:12:50 +0900

    스코어 개구리 슈퍼점프, 방향전환해결 + 밟힘 애니메이션 진행중

--- Tadpole/Assets/Script/ScoreFrog/ScoreMode_Frog_PlayController_Jump.cs
+++ Tadpole/Assets/Script/ScoreFrog/ScoreMode_Frog_PlayController_Jump.cs
@@ -6,26 +6,34 @@ public class ScoreMode_Frog_PlayController_Jump : MonoBehaviour
 {
    
     public Rigidbody2D rigid;
+    SpriteRenderer spriteRenderer;
     public float jumpPower;
+    bool left,right;
     // Start is called before the first frame update
     void Start()
     {
         rigid = GetComponent<Rigidbody2D>();
-        jumpPower = 17;
+        jumpPower = 10;
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        left = GameObject.Find("Left").GetComponent<ScoreMode_Frog_PlayerController_LeftRight>().isPressed;
+        right = GameObject.Find("Right").GetComponent<ScoreMode_Frog_PlayerController_LeftRight>().isPressed;
+        if (left)
+            spriteRenderer.flipX = true;
+        if (right)
+            spriteRenderer.flipX = false;
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.tag == "ScoreMode_Frog_Enemy")
         {
-            rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
-           // Debug.Log("Ads");
+            //rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            rigid.velocity = new Vector2(0, jumpPower);
         }
     }
 }
