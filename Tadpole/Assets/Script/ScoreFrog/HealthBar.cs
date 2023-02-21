57b067e09d945e9befdf5d7ef9fb9c8988eac071
leech1211 <leech1211@nate.com>
2023-02-21 14:03:31 +0900

    스코어 개구리 생명력 완료

--- Tadpole/Assets/Script/ScoreFrog/HealthBar.cs
+++ Tadpole/Assets/Script/ScoreFrog/HealthBar.cs
@@ -6,20 +6,30 @@ using UnityEngine.UI;
 public class HealthBar : MonoBehaviour
 {
     public Slider slider;
+    public Gradient gradient;
+    public Image fill;
 
     private void Start()
     {
         slider.maxValue = 20;
         slider.value = 20;
+        fill.color = gradient.Evaluate(1f);
         Invoke("decreaseHealth", 1);
     }
+    /*private void Update()
+    {
+        if (slider.value >= 20)     //최대체력
+            slider.value = 20;
+    }*/
     public void decreaseHealth()
     {
         slider.value -= 1;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
         Invoke("decreaseHealth", 1);
     }
     public void getHealth()
     {
         slider.value += 10;
+        fill.color = gradient.Evaluate(slider.normalizedValue);
     }
 }
