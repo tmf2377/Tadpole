1161af56e7e56cdd3257933b23bfe7361badb7b8
leech1211 <leech1211@nate.com>
2023-02-26 16:18:45 +0900

    스코어 개구리 게임오버 패널 및 최종점수

---
+++ Tadpole/Assets/Script/ScoreFrog/Score_Frog_Gameover_Score.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score_Frog_Gameover_Score : MonoBehaviour
+{
+    float finalScore;
+    Text scoreText;
+    
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        finalScore = GameObject.Find("Player").GetComponent<ScoreMode_Frog_PlayController_Jump>().finalScore;
+        scoreText.text = "Score : " + finalScore.ToString();
+    
+    }
+}
