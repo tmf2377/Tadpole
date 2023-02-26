2964dda099eb29605ced85a594060e2d1ea506a6
leech1211 <leech1211@nate.com>
2023-02-26 17:22:34 +0900

    스코어 개구리 게임오버 애니메이션

--- Tadpole/Assets/Script/ScoreFrog/Score_Frog_Gameover.cs
+++ Tadpole/Assets/Script/ScoreFrog/Score_Frog_Gameover.cs
@@ -5,6 +5,19 @@ using UnityEngine.SceneManagement;
 
 public class Score_Frog_Gameover : MonoBehaviour
 {
+    bool isGameOver;
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        isGameOver = GameObject.Find("Player").GetComponent<ScoreMode_Frog_PlayController_Jump>().isGameOver;
+        anim.SetBool("isGameOver", isGameOver);
+    }
     public void RePlay()
     {
         SceneManager.LoadScene("Frog_intro");
