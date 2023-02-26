08e6e7184d57d49fa3d4d528c972b2588fe7d568
leech1211 <leech1211@nate.com>
2023-02-26 14:32:03 +0900

    스코어 개구리 바람 효과 삽입

---
+++ Tadpole/Assets/Script/ScoreFrog/WindEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindEffect : MonoBehaviour
+{
+
+   Rigidbody2D rigidbody;
+    int speed;
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        speed = -1;
+        Invoke("Delete", 0.6f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigidbody.velocity = new Vector2(speed, 0);      // 그 고도에서 비행
+    }
+    void Delete()
+    {
+        Destroy(gameObject);
+    }
+
+}
---
+++ Tadpole/Assets/Script/ScoreFrog/WindEffectSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindEffectSpawner : MonoBehaviour
+{
+    public Transform[] spawnPoints;
+    public GameObject windEffect;
+    int spawnTime;
+    void Start()
+    {
+        spawnTime = 0;
+    }
+
+    void Update()
+    {
+        //spawnTime += Time.deltaTime;
+        spawnTime++;
+        if (spawnTime % 15 == 0)
+        {
+            int randSpawnPoint = Random.Range(0, spawnPoints.Length);
+
+            Instantiate(windEffect, spawnPoints[randSpawnPoint].position, transform.rotation);
+        }
+    }
+
+   
+}
